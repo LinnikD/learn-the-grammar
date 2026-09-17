3a1d01a7dd4e3a1f6338824c3ee57a260ffc9a7e
linnikd <lindeni@mail.ru>
2026-09-17 22:25:37 +0300

    fix for report

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
 
   retries: process.env.CI ? 1 : 0,
 
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+  ],
+
   use: {
     baseURL: 'http://localhost',
     trace: 'on-first-retry',
